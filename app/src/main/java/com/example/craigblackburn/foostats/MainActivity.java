19e37e017483ae8e7d3a9c95f4c6540a152e3e4c
Craig Blackburn <cr.blackburn89@gmail.com>
2016-12-12 12:01:06 -0700

    Added facebook login fragment

--- app/src/main/java/com/example/craigblackburn/foostats/MainActivity.java
+++ app/src/main/java/com/example/craigblackburn/foostats/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.craigblackburn.foostats;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FacebookLoginFragment facebookLoginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(getApplication());
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            facebookLoginFragment = new FacebookLoginFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.com_facebook_fragment_container, facebookLoginFragment)
+                    .commit();
+        } else {
+            facebookLoginFragment = (FacebookLoginFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.com_facebook_fragment_container);
+        }
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        facebookLoginFragment.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
